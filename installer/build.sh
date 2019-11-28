dc8453cc834752951de49bc752999b9f15f6d1d3
Rodolphe Pineau <pineau@rti-zone.org>
2019-11-28 15:27:46 -0800

    Fix build script

--- installer/build.sh
+++ installer/build.sh
@@ -4,12 +4,15 @@ mkdir -p ROOT/tmp/m1OASYS_X2/
 cp "../domelist m1OASYS.txt" ROOT/tmp/m1OASYS_X2/
 cp "../build/Release/libm1OASYS.dylib" ROOT/tmp/m1OASYS_X2/
 
+PACKAGE_NAME="m1OASYS_X2.pkg"
+BUNDLE_NAME="org.rti-zone.m1OASYSX2"
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.m1OASYS_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 m1OASYS_X2.pkg
-pkgutil --check-signature ./m1OASYS_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.m1OASYS_X2 --scripts Scripts --version 1.0 m1OASYS_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 DomePro_X2.pkg
 fi
 
 rm -rf ROOT
