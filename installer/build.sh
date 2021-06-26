e27150b08010edb3cc056e4654abcf0409751672
Rodolphe Pineau <pineau@rti-zone.org>
2021-06-26 11:31:20 -0700

    fix missing windows dll static linking for 64 bits

--- installer/build.sh
+++ installer/build.sh
@@ -17,7 +17,7 @@ if [ ! -z "$installer_signature" ]; then
 	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
 	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 DomePro_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
